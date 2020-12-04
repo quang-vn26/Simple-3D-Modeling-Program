1b6d76876722d59736eafbc13d6bf56051660e81
Quangg <65973279+quang-vn26@users.noreply.github.com>
2020-12-04 17:53:42 +0700

    3d scene

--- run.cpp
+++ run.cpp
@@ -9,9 +9,19 @@ Change Color
 #include<iostream>
 #include<GL/glut.h>
 using namespace std;
+void theCube(){
+	//mau trang
+	glColor3f(1,1,1);
+	//ve hinh cube
+	glutSolidCube(0.4);
+}
 void display(){
+	// xoay 1 goc 40 do, theo truc x,y gio nguyen
+	glRotatef(40,1,1,0);
+	theCube();
 	glutSwapBuffers();
 }
+
 int main(int argc, char ** argv){
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_DOUBLE);
