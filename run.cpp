5412047a93c3ab9f52e8bf9ce1b1343c9bb21335
Quangg <65973279+quang-vn26@users.noreply.github.com>
2020-12-04 17:47:34 +0700

    add 3d windows

--- run.cpp
+++ run.cpp
@@ -6,6 +6,21 @@ Move a Cube
 Create Quads
 Change Color
 */
+#include<iostream>
+#include<GL/glut.h>
+using namespace std;
+void display(){
+	glutSwapBuffers();
+}
+int main(int argc, char ** argv){
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(800,600);
+	glutCreateWindow("");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
 
 
 
